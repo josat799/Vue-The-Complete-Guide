965850129915c3dd3b539964c663914cc17a401e
Josef Atoui - MAC <josef.atoui@live.se>
2021-04-26 13:34:01 +0200

    Added event listeners

--- project3/app.js
+++ project3/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+    inputName(event, lastname) {
+      this.name = event.target.value + ' ' + lastname;
+    }
+  }
 });
 
 app.mount('#events');
